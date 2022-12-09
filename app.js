dcf898306768e2f8c8d1f07e5105208dc7d09500
Ankit Verma <vermaankit004@gmail.com>
2022-12-09 13:07:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3001;
 
 app.get("/", (req, res) => res.type('html').send(html));
 
-app.get("/customJob", (req, res) => {
+app.post("/customJob", (req, res) => {
     //const msg = "unauthorized";
     //res.status(401).send(msg);
     //res.type('html').send("ok:customJob");
