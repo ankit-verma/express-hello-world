695bb04cadce5c6ed237e3547642495ae4e86975
Ankit Verma <vermaankit004@gmail.com>
2022-12-09 23:34:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,75 +1,103 @@
 const express = require("express");
 const bodyParser = require('body-parser')
 
+const axios = require("axios");
 const app = express();
+
+const storeConfig = {
+    url: "99apps.myshopify.com",
+    apiVersion: "2022-10",
+    token: "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" 
+}
 
 // create application/json parser
 var jsonParser = bodyParser.json()
  
 // create application/x-www-form-urlencoded parser
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
+ 
+// POST /login gets urlencoded bodies
+//app.post('/login', urlencodedParser, function (req, res) {
+  //res.send('welcome, ' + req.body.username)
+//})
+ 
+// POST /api/users gets JSON bodies
+//app.post('/api/users', jsonParser, function (req, res) {
+  // create user in req.body
+//})
+
+
+//app.get("/", (req, res) => res.type('html').send(html));
 
 app.post("/customJob", jsonParser, async (req, res) => {
+
     const token = req.headers.token;
     //const msg = "unauthorized";
     //res.status(401).send(msg);
     //res.type('html').send("ok:customJob");
     const data = req.body;
-    console.log("token",token);
-    console.log("data",data);
-    res.status(200).send(data);
+    //console.log("token",token);
+    //console.log("data",data);
+    let response = "not invoked";
+
+    if(data.variants[0].inventory_quantity < 1 && data.status == "active"){
+        response = await productUpdate(data.id);
+    }
+    res.status(200).send(response);
 });
 
+async function productUpdate(id){
+    const productId = id;
+    
+    const url = `https://${storeConfig.url}/admin/api/${storeConfig.apiVersion}/products/${productId}.json`;
+
+    const payload = `{
+        "product":{"id":${productId},"status":"draft"}
+    }`;
+
+    const headers = {
+        'Content-Type': 'application/json',
+        'X-Shopify-Access-Token': storeConfig.token
+    };
+    //console.log("payload",payload);
+    //console.log("url",url);
+    //console.log("payload",payload);
+
+    const config = {
+        method: 'put',
+        url: url,
+        headers: headers,
+        data: payload
+    };
+
+    try {
+        const response = await axios(config);
+        const data = response.data;
+
+        if(data && data.id && data.id.length > 0){
+            return {
+                status: 200,
+                statusText: 'sent to shopify',
+                data: data
+            }
+        }
+        else {
+            return {
+                status: 500,
+                statusText: 'error sending to shopify'
+            }
+        }
+    } catch (err) {
+        return {
+            status: 500,
+            statusText: 'error sending to shopify',
+            errors: err
+        }
+    }
+}
+      
 const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
-      }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
-</html>`;
+const html = `ok`;
